c15c6e8174bb9daadd0bdb2b2a9b2cd5e833f36b
ShadZire <gmanu21097@gmail.com>
2018-08-24 20:56:29 +0530

    C programs in 1 file

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(int argc, char **argv )
+{
+   /*
+    printf("Hello world!\n");
+    printf("A decimal number - %d\n", 5);
+    printf("A floating point number - %f\n", 5.0);
+    printf("A decimal number - %d\n", 6);
+    printf("A floating point number - %f", 6.0);
+    */
+
+    /*
+    int i = 0;
+    bool Manu = false;
+    if(Manu){
+        for(i=0; i<120;i++){
+            printf("%d = %c\n", i,i);
+        }
+    }
+    */
+
+    /*
+    int i = 0;
+    i++;
+    printf(" %d", i+=1);
+
+      return 0;
+    */
+
+    int a;
+    int num1 = 5,num2;
+    scanf(" %d", &a);
+    if(num1 > (num2 = a)){
+        printf(" num1 is greater than you");
+    }
+
+    return 0;
+}
